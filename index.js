2c0970216784aa660e8e0e531a78868a8faaf11d
Arthur Ish <arthurishdev@gmail.com>
2024-01-09 22:31:39 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+// Write your code herelet num
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(2,4,5,20);
